a2b1832e334da9aa4287402313caee3d3b474736
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-11-08 13:49:22 +0000

    fixed issue with css notloading, changed navbar and hero img, and fixed issue with products media root not loading in the correct folder

--- petalia_flowershop/settings.py
+++ petalia_flowershop/settings.py
@@ -151,6 +151,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

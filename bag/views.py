edf8e8d83cfe31b70166526bccb6f19b245d42ee
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-11-12 14:22:32 +0000

    fixed issue with update button in the bag/checkout

--- bag/views.py
+++ bag/views.py
@@ -7,70 +7,77 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
-from django.shortcuts import redirect, get_object_or_404
-from products.models import Product
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-    
-    # Attempt to get quantity, default to 1 if missing or invalid
-    try:
-        quantity = int(request.POST.get('quantity', 1))
-    except (TypeError, ValueError):
-        quantity = 1  # Default to 1 if quantity is invalid
 
-    redirect_url = request.POST.get('redirect_url', '/')
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    # If item already in bag, increase quantity; otherwise, add it
-    if item_id in bag:
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
-    # Update session with new bag contents
     request.session['bag'] = bag
-    
-    # Redirect to the specified URL or a default page if URL is missing
     return redirect(redirect_url)
-
     
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
-    # Try to retrieve and validate quantity; default to 0 if invalid
-    try:
-        quantity = int(request.POST.get('quantity', 0))
-    except (TypeError, ValueError):
-        quantity = 0
-
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    # Adjust the quantity or remove item based on quantity
-    if quantity > 0:
-        bag[item_id] = quantity  
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
     else:
-        bag.pop(item_id, None)  
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
 
-    # Update the session with the adjusted bag
     request.session['bag'] = bag
-    
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
-    """Remove the specified item from the shopping bag"""
-    
+    """Remove the item from the shopping bag"""
+
     try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        # Remove the item from the bag
-        bag.pop(item_id, None)
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
 
-        # Update the session with the modified bag
         request.session['bag'] = bag
-
         return HttpResponse(status=200)
 
     except Exception as e:

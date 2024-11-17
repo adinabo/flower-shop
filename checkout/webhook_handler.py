d7a3404e23cd88f5b7dfb7aa8af440176a89ea0d
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-11-17 09:42:43 +0000

    added StripeWH_Handler class

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    

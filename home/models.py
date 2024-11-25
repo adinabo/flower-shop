99919757b0ab8c8547506d890edb627f0d6676eb
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-11-25 12:48:39 +0000

    fixed issue with iteration over products on the index page. collections are showing properly now

--- home/models.py
+++ home/models.py
@@ -1,3 +1,2 @@
-from django.db import models
+from products.models import Product, Category
 
-# Create your models here.
--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
-    """ return index page view"""
     
-    return render(request, 'home/index.html')
+    """Return the index page with a collections section."""
+    
+    # Fetch random products for each category
+    bouquets_products = Product.objects.filter(category__name='bouquets').order_by('?')[:6]
+    bridal_products = Product.objects.filter(category__name='bridal').order_by('?')[:6]
+
+    context = {
+        'bouquets_products': bouquets_products,
+        'bridal_products': bridal_products,
+    }
+    return render(request, 'home/index.html', context)
 
 
 def about(request):

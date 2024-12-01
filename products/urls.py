122485ae3838e6c99cdb9c2653302031ed5e9475
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-12-01 15:27:58 +0000

    added add_product html and url

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
     path('bag/add/<int:product_id>/', views.add_to_bag, name='add_to_bag_product'),
+    path('add/', views.add_product, name='add_product'),
 ]

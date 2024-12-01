3867e3c017562162f4f4d27cdec37261784ef237
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-12-01 12:21:46 +0000

    added order history, fixed issue with profile view

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 from .models import UserProfile
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -9,6 +9,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.userprofile.save()
+        # Only create a new profile if it doesn't exist
+        UserProfile.objects.get_or_create(user=instance)
+    else:
+        # Update the profile for an existing user (if necessary)
+        instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .forms import UserProfileForm 
 from .models import UserProfile
 from django.contrib.auth.decorators import login_required
@@ -6,18 +7,11 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     """ Display the user's profile. """
-    # Try to get the user's profile, and redirect to a profile creation view if not found
-    try:
-        profile = UserProfile.objects.get(user=request.user)
-    except UserProfile.DoesNotExist:
-        return redirect('create_profile')  # Ensure you have a create_profile view for profile creation
+    # Get the user's profile or return 404 if not found
+    profile = get_object_or_404(UserProfile, user=request.user)
 
-    template = 'profiles/profile.html'
-    context = {
-        'profile': profile,
-    }
-
-    return render(request, template, context)
+    # Use the template directly
+    return render(request, 'profiles/profile.html', {'profile': profile})
 
 
 def create_profile(request):
@@ -60,3 +54,20 @@ def create_profile(request):
         form = UserProfileForm()
 
     return render(request, 'profiles/create_profile.html', {'form': form})
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)

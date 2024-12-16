6d65e4dbb4497267847761fb47ec9941b092a2d1
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-12-16 09:19:50 +0000

    added social icons, added success message for newsletter

---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('newsletter-form').addEventListener('submit', function(e) {
+        e.preventDefault(); 
+        document.getElementById('success-message').style.display = 'block'; 
+    });
